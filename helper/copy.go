6ddda99d3c66ee64eb0dd5d85164ac340f642a58
kirari04 <lev@saminskij.ch>
2024-02-23 16:30:19 +0100

    improoved progress message

--- helper/copy.go
+++ helper/copy.go
@@ -3,54 +3,57 @@ package helper
 import (
 	"io"
 	"os"
+	"time"
 
 	"github.com/labstack/gommon/log"
 )
 
-func Copy(srcpath, dstpath string) (err error) {
-	r, err := os.Open(srcpath)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	w, err := os.Create(dstpath)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if c := w.Close(); err == nil {
-			err = c
-		}
-	}()
-
-	// Create a multi writer to write to both the file and the logger
-	multiWriter := io.MultiWriter(w, os.Stdout)
-
-	// Create a proxy reader to intercept the data being read
-	proxyReader := &ProxyReader{
-		Reader:   r,
-		Progress: logProgress,
-	}
-
-	_, err = io.Copy(multiWriter, proxyReader)
-	return err
+// ProgressWriter wraps an io.Writer and prints progress information.
+type ProgressWriter struct {
+	io.Writer
+	Total   int64 // Total bytes to copy
+	Copied  int64 // Bytes copied so far
+	LastMsg time.Time
 }
 
-type ProxyReader struct {
-	io.Reader
-	Progress func(copied, total int64)
-	Copied   int64
-	Total    int64
-}
-
-func (pr *ProxyReader) Read(p []byte) (n int, err error) {
-	n, err = pr.Reader.Read(p)
-	pr.Copied += int64(n)
-	pr.Progress(pr.Copied, pr.Total)
+func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
+	n, err = pw.Writer.Write(p)
+	pw.Copied += int64(n)
+	progress := float64(pw.Copied) / float64(pw.Total) * 100
+	if time.Since(pw.LastMsg).Seconds() > 2 {
+		pw.LastMsg = time.Now()
+		log.Infof("Copier in Progress... %.2f%%", progress)
+	}
 	return
 }
 
-func logProgress(copied, total int64) {
-	log.Infof("Copying... %d/%d bytes", copied, total)
+func Copy(srcpath, dstpath string) (err error) {
+	srcFile, err := os.Open(srcpath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	srcFileInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	dstFile, err := os.Create(dstpath)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	progressWriter := &ProgressWriter{
+		Writer: dstFile,
+		Total:  srcFileInfo.Size(),
+		Copied: 0,
+	}
+
+	_, err = io.Copy(progressWriter, srcFile)
+	if err != nil {
+		return err
+	}
+	return nil
 }
