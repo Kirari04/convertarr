708df2cf0d75ad0d23f44a3e53c9489be99a5a4b
kirari04 <lev@saminskij.ch>
2024-02-23 16:22:36 +0100

    added copy progress tracking

--- helper/copy.go
+++ helper/copy.go
@@ -3,6 +3,8 @@ package helper
 import (
 	"io"
 	"os"
+
+	"github.com/labstack/gommon/log"
 )
 
 func Copy(srcpath, dstpath string) (err error) {
@@ -10,21 +12,45 @@ func Copy(srcpath, dstpath string) (err error) {
 	if err != nil {
 		return err
 	}
-	defer r.Close() // ignore error: file was opened read-only.
+	defer r.Close()
 
 	w, err := os.Create(dstpath)
 	if err != nil {
 		return err
 	}
-
 	defer func() {
-		// Report the error, if any, from Close, but do so
-		// only if there isn't already an outgoing error.
 		if c := w.Close(); err == nil {
 			err = c
 		}
 	}()
 
-	_, err = io.Copy(w, r)
+	// Create a multi writer to write to both the file and the logger
+	multiWriter := io.MultiWriter(w, os.Stdout)
+
+	// Create a proxy reader to intercept the data being read
+	proxyReader := &ProxyReader{
+		Reader:   r,
+		Progress: logProgress,
+	}
+
+	_, err = io.Copy(multiWriter, proxyReader)
 	return err
+}
+
+type ProxyReader struct {
+	io.Reader
+	Progress func(copied, total int64)
+	Copied   int64
+	Total    int64
+}
+
+func (pr *ProxyReader) Read(p []byte) (n int, err error) {
+	n, err = pr.Reader.Read(p)
+	pr.Copied += int64(n)
+	pr.Progress(pr.Copied, pr.Total)
+	return
+}
+
+func logProgress(copied, total int64) {
+	log.Infof("Copying... %d/%d bytes", copied, total)
 }
