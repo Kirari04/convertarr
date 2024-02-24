25beda31e40affee44b403f1a43238919c76dd9d
kirari04 <lev@saminskij.ch>
2024-02-24 19:14:54 +0100

    improve copy speed by not checking time comparison on each copy

--- helper/copy.go
+++ helper/copy.go
@@ -11,19 +11,16 @@ import (
 // ProgressWriter wraps an io.Writer and prints progress information.
 type ProgressWriter struct {
 	io.Writer
-	Total   int64 // Total bytes to copy
-	Copied  int64 // Bytes copied so far
-	LastMsg time.Time
+	Total        int64 // Total bytes to copy
+	Copied       int64 // Bytes copied so far
+	SrcPath      string
+	DstPath      string
+	StopTracking bool
 }
 
 func (pw *ProgressWriter) Write(p []byte) (n int, err error) {
 	n, err = pw.Writer.Write(p)
 	pw.Copied += int64(n)
-	progress := float64(pw.Copied) / float64(pw.Total) * 100
-	if time.Since(pw.LastMsg).Seconds() > 2 {
-		pw.LastMsg = time.Now()
-		log.Infof("Copier in Progress... %.2f%%", progress)
-	}
 	return
 }
 
@@ -46,14 +43,31 @@ func Copy(srcpath, dstpath string) (err error) {
 	defer dstFile.Close()
 
 	progressWriter := &ProgressWriter{
-		Writer: dstFile,
-		Total:  srcFileInfo.Size(),
-		Copied: 0,
+		Writer:       dstFile,
+		Total:        srcFileInfo.Size(),
+		Copied:       0,
+		SrcPath:      srcpath,
+		DstPath:      dstpath,
+		StopTracking: false,
 	}
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			if progressWriter.StopTracking {
+				break
+			}
+			progress := float64(progressWriter.Copied) / float64(progressWriter.Total) * 100
+			log.Infof("Copier in Progress on file [%s] : %.2f%%", progressWriter.SrcPath, progress)
+		}
+	}()
 
 	_, err = io.Copy(progressWriter, srcFile)
 	if err != nil {
 		return err
 	}
+
+	progressWriter.StopTracking = true
+
 	return nil
 }
